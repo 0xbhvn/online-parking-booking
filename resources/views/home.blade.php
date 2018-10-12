c606472ba19d4cd3ffe9abcc149fd7d1ce7a5dbe
Bhaven Kakade <thisisbhaven@gmail.com>
2018-10-12 20:51:10 +0530

    final

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,42 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Personal Details</div>
+<h3 style="padding-left:80px">Your Details</h3><br />
+<p style="padding-left:80px; font-size:20px;">
+    Name: <strong>{{ auth()->user()->name }}</strong><br />
+    Vehicle Id: <strong>{{ auth()->user()->vehicle_no }}</strong><br />
+    Email: <strong>{{ auth()->user()->email }}</strong><br />
+    Moodle Id: <strong>{{ auth()->user()->moodle }}</strong><br />
+</p>
+<br />
 
-                <div class="card-body">
-                    Name: <strong>{{ auth()->user()->name }}</strong><br>
-                    Email Id: <strong>{{ auth()->user()->email }}</strong><br>
-                    Moodle Id: <strong>{{ auth()->user()->moodle }}</strong><br>
-                    Vehicle No: <strong>{{ auth()->user()->vehicle_no }}</strong><br>
-                </div>
-            </div>
+<hr>
+<br />
+<h3 style="padding-left:80px">Your Pass</h3><br />
+<p style="padding-left:80px;font-size:20px;">
+    @if($booking)
+        <?php $carbon = new \Carbon\Carbon($booking->created_at); ?>
+        @if($carbon->addDays($booking->validity) > \Carbon\Carbon::now())
+            Slot: <strong>{{ $booking->slot->area }} - {{ $booking->slot->slot }}</strong><br>
+            Valid till: <strong>{{ $carbon->toFormattedDateString() }}</strong><br>
+            Valid from: <strong>{{ $carbon->subDays($booking->validity)->toFormattedDateString() }}</strong>
+        @else
+            <a href="/booking"><button type="button" class="btn btn-primary">Book</button></a>
+        @endif
+        <br>
+    @else
+        <a href="/booking"><button type="button" class="btn btn-primary">Book</button></a>
+@endif
+        <br />
+<hr>
+<br />
 
-            <hr>
-
-            <div class="card">
-                <div class="card-header">Current Pass</div>
-
-                <div class="card-body">
-                    @if($booking)
-                        Slot: <strong>{{ $booking->slot->area }} - {{ $booking->slot->slot }}</strong><br>
-                        <?php $carbon = new \Carbon\Carbon($booking->created_at); ?>
-                        @if($carbon->addDays($booking->validity) > \Carbon\Carbon::now())
-                            Valid till: <strong>{{ $carbon->toFormattedDateString() }}</strong><br>
-                            Valid from: <strong>{{ $carbon->subDays($booking->validity)->toFormattedDateString() }}</strong>
-                        @else
-                            <a href="/booking"><button type="button" class="btn btn-primary">Book</button></a>
-                        @endif
-                        <br>
-                    @else
-                        <a href="/booking"><button type="button" class="btn btn-primary">Book</button></a>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
